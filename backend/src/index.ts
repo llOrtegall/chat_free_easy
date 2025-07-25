b839e22a881652c6a434dcf1eeda24f653b26262
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-25 16:50:57 -0500

    add firts run express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("hello word");
+import express, { json, type Request, type Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('test ok')
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on: http://localhost:' + PORT );
+})
