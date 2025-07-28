d2888d1d3d7d238559f852d96a413b9179dc01c7
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-28 16:42:10 -0500

    example login and logout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import { signIn, auth, signOut } from "@/auth";
+
+export default async function Home() {
+  const session = await auth()
+
   return (
-    <div className="bg-red-800">
-      hi
-    </div>
-  );
+
+    !session ? (
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+      >
+        <button type="submit" className="p-2 bg-blue-500 text-white rounded m-2 hover:bg-blue-600 cursor-pointer">Signin with Google</button>
+      </form>
+    ) : (
+      <div>
+        <p>Logged in as {session.user?.email}</p>
+        <form action={async () => {
+          "use server"
+          await signOut()
+        }}>
+          <button type="submit" className="p-2 bg-blue-500 text-white rounded m-2 hover:bg-blue-600 cursor-pointer">Sign out</button>
+        </form>
+      </div>
+    )
+
+  )
+
 }
