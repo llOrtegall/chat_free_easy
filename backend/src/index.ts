c7f3a1310c0daf321fc9930946856c37d4cbdcc0
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-31 15:01:32 -0500

    implements firts websocket server whit express and node js

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,28 @@
-import express, { json, type Request, type Response } from "express";
+import express, { type Request, type Response } from "express";
+import { WebSocketServer } from "ws";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.disable('x-powered-by')
+    .use(express.urlencoded({ extended: true }))
+    .use(express.json())
+    .use(morgan('dev'))
+    .use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.status(200).send('test ok')
 })
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log('Server running on: http://localhost:' + PORT );
+})
+
+const wss = new WebSocketServer({ server, path: '/ws' })
+
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+    console.log(ws);
 })
