1eb7c4348936797ee8b3704082774bf498c63164
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-07-31 15:46:01 -0500

    feat: implement user presence tracking and online status notifications via WebSocket

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { type Request, type Response } from "express";
-import { WebSocketServer } from "ws";
+import WebSocket, { WebSocketServer } from "ws";
+import { v4 as uuid } from "uuid";
 import morgan from "morgan";
 import cors from "cors";
 
@@ -7,22 +8,79 @@ const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.disable('x-powered-by')
-    .use(express.urlencoded({ extended: true }))
-    .use(express.json())
-    .use(morgan('dev'))
-    .use(cors());
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(morgan('dev'))
+  .use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('test ok')
+  res.status(200).send('test ok')
 })
 
 const server = app.listen(PORT, () => {
-    console.log('Server running on: http://localhost:' + PORT );
+  console.log('Server running on: http://localhost:' + PORT);
 })
 
 const wss = new WebSocketServer({ server, path: '/ws' })
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    console.log(ws);
+interface DataWs extends WebSocket {
+  id?: string;
+  name?: string;
+  email?: string;
+  image?: string;
+}
+
+interface DataMessage {
+  type: string;
+  name: string;
+  email: string;
+  image: string;
+}
+
+const getOnlineUsers = () =>
+  [...wss.clients]
+    .map(c => {
+      const s = c as DataWs;
+      return {
+        id: s.id,
+        name: s.name,
+        image: s.image,
+        email: s.email,
+      };
+    })
+    .filter(u => Boolean(u.id && u.name && u.image && u.email));
+
+const notifyOnlineUsers = () => {
+  const payload = JSON.stringify({
+    type: 'online_users',
+    data: getOnlineUsers(),
+  });
+
+  for (const client of wss.clients) {
+    try { client.send(payload); } catch { }
+  }
+};
+
+wss.on('connection', (ws: DataWs) => {
+
+  ws.on('message', (data) => {
+    const message = JSON.parse(data.toString());
+
+    if (message instanceof Object && 'type' in message) {
+      const newData: DataMessage = message;
+      if (newData.type === 'join') {
+        ws.id = uuid();
+        ws.name = newData.name;
+        ws.email = newData.email;
+        ws.image = newData.image;
+
+        notifyOnlineUsers();
+      }
+    }
+
+    ws.on('close', () => {
+      notifyOnlineUsers();
+    });
+
+  })
 })
