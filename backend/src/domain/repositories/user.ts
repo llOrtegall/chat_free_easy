2d4b2bb96a02d958be0c5d54bbe034af0c4d736a
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-01 18:59:15 -0500

    refactor: remove unused UserRepository interface from domain layer

--- backend/src/domain/repositories/user.ts
+++
@@ -1,7 +0,0 @@
-import type { UserEntity } from "../entities/user.js";
-
-export interface UserRepository {
-    findUserById(uuid: string): Promise<UserEntity | null>
-    registerUser(user: UserEntity): Promise<UserEntity | null>
-    listUser(): Promise<UserEntity[] | []>
-}
