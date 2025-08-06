9a9441ad1b5d5bed9f590b7ce8ca1842955f0f7a
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-06 15:05:06 -0500

    feat: add SignIn component to home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,10 @@
+import { SignIn } from "@/app/components/auth/signin-button";
+
 export default function Home() {
   return (
     <div className="font-sans bg-gray-900">
       <h1>Chat Free Easy</h1>
+      <SignIn />
     </div>
   );
 }
