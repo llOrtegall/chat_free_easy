46065054363acfe50d247308e6640bdc865038b3
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-07 17:22:47 -0500

    feat: add chat component and style dashboard layout with dark theme

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { SignOut } from "@/app/components/auth/signout-button"
 import { redirect } from "next/navigation"
 import { auth } from "@/auth"
+import { Chat } from "../components/Chat"
 
 export default async function Dashboard() {
   const session = await auth()
@@ -8,10 +9,13 @@ export default async function Dashboard() {
   if (!session) redirect("/")
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Session: {JSON.stringify(session)}</p>
-      <SignOut />
+    <div className="h-screen bg-gray-950">
+      <header className="flex justify-between p-2">
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+        <SignOut />
+      </header>
+
+      <Chat />
     </div>
   )
 }
