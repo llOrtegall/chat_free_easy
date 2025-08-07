557bec72154c75a9acac52ab8463cca5fbf6cf68
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-07 17:22:28 -0500

    feat: add GitHub sign-in option alongside existing Google authentication

--- frontend/app/components/auth/signin-button.tsx
+++ frontend/app/components/auth/signin-button.tsx
@@ -1,14 +1,24 @@
 import { signIn } from "@/auth"
- 
+
 export function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google", { redirectTo: "/dashboard" })
-      }}
-    >
-      <button type="submit">Sign in with Google</button>
-    </form>
+    <>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google", { redirectTo: "/dashboard" })
+        }}
+      >
+        <button type="submit">Sign in with Google</button>
+      </form>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github", { redirectTo: "/dashboard" })
+        }}
+      >
+        <button type="submit">Sign in with GitHub</button>
+      </form>
+    </>
   )
 }
