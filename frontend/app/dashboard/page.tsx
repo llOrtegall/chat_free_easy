58092f18dfd992e41ce910e2c827b84c70239de0
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-11 18:48:50 -0500

    feat: add user avatar and name to dashboard header

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -2,19 +2,25 @@ import { SignOut } from "@/app/components/auth/signout-button"
 import { redirect } from "next/navigation"
 import { auth } from "@/auth"
 import { Chat } from "../components/Chat"
+import Image from "next/image"
 
 export default async function Dashboard() {
   const session = await auth()
 
   if (!session || !session.user) redirect("/")
-  
+
   return (
     <div className="h-screen bg-gray-950">
-      <header className="flex justify-between p-2">
-        <h1 className="text-2xl font-bold">Dashboard</h1>
+      <header className="flex justify-between items-center space-x-2 p-2 bg-gray-700">
+
+        <div className="flex items-center space-x-2">
+          <Image src={session.user.image || ""} alt={session.user.name || ""} width={32} height={32} className="w-8 h-8 rounded-full" />
+          <p className="text-white">{session.user.name}</p>
+        </div>
         <SignOut />
+
       </header>
-      <Chat 
+      <Chat
         email={session.user.email || ""}
         name={session.user.name || ""}
         image={session.user.image || ""}
