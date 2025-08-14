6f03a1b77488ed36e4001a875762342a94075ca8
llOrTeGall <IvanOrtega_97@hotmail.com>
2025-08-14 14:05:02 -0500

    refactor: update auth environment variables and add GitHub OAuth provider

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,16 +1,18 @@
 import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
- 
+import GitHub from "next-auth/providers/github"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
     GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
     }),
   ],
+  trustHost: true,
+  secret: process.env.AUTH_SECRET,
 })
